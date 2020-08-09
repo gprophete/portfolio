2aad2c23c079a54fbe91623acb215ef0ac3b665e
gprophete <garlyprophete30@gmail.com>
2020-08-08 20:36:06 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home.js'
+
+function App() {
+  return (
+    <div>
+    <h1>Portfolio</h1>
+    <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class Home extends React.Component {
+    render(){
+        return(
+            <div>
+            <div>My Name is Garly Prophete</div>
+            <div>I am a software developer</div>
+            </div>
+
+        )
+    }
+
+}
